34d0bf15b84af9770facb8b64f133d2c37f7ea99
DexterKim <dexjkim@gmail.com>
2023-06-17 17:57:01 -0700

    Initial Commit

---
+++ SwiftCoinTutorial/SwiftCoinTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinTutorialApp.swift
+//  SwiftCoinTutorial
+//
+//  Created by Dexter Kim on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
